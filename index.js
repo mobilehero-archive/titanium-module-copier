a03332abe47f1c7a3cc994dd5b0670d19058c38a
Chris Williams <chris.a.williams@gmail.com>
2019-05-17 17:18:26 -0400

    refactor: include optional dependencies, make async, copy in parallel

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
 const copier = {};
 module.exports = copier;
 
-const _ = require('lodash');
+const get = require('lodash.get');
 require('colors');
 const fs = require('fs-extra');
 const path = require('path');
 const NODE_MODULES = 'node_modules';
 
-copier.execute = (projectPath, targetPath) => {
-	if (_.isNil(projectPath)) {
+/**
+ * @param {string} projectPath absolute filepath for project root directory
+ * @param {string} targetPath absolute filepath for target directory to copy node_modules into
+ * @returns {Promise<void>}
+ */
+copier.execute = async (projectPath, targetPath) => {
+	if (projectPath === null) {
 		throw new Error('projectPath must be defined.');
-	} else if (_.isNil(targetPath)) {
+	}
+	if (targetPath === null) {
 		throw new Error('targetPath must be defined.');
 	}
 
@@ -20,30 +26,47 @@ copier.execute = (projectPath, targetPath) => {
 
 	let packageJson;
 	try {
-		packageJson = require(path.join(projectPath, 'package.json'));
+		packageJson = await fs.readJson(path.join(projectPath, 'package.json'));
 	} catch (err) {
 		console.error(err);
-		return new Error(`Cannot load package.json file: ${path.join(projectPath, 'package.json')}`);
+		throw new Error(`Cannot load package.json file: ${path.join(projectPath, 'package.json')}`);
 	}
 
 	const dependencies = packageJson && packageJson.dependencies;
+	const directoriesToBeCopied = gatherDirectoriesToCopy(projectPath, Object.keys(dependencies));
+	console.debug(`directoriesToBeCopied: ${JSON.stringify(directoriesToBeCopied, null, 2)}`.blue);
 
+	return Promise.all(directoriesToBeCopied.map(async directory => {
+		const destPath = path.join(targetPath, directory.substring(projectPath.length));
+		console.debug(`copying to directory: ${destPath}`);
+		return fs.copy(directory, destPath, { overwrite: true });
+	}));
+};
+
+// FIXME: Use an actual Set to ensure no duplicates?
+/**
+ * Gathers the full listing of directories we need to copy
+ * @param {string} projectPath absolute path to source project root directory
+ * @param {string[]} dependencies array of module ids to be copied
+ * @returns {string[]} set of directories to copy
+ */
+function gatherDirectoriesToCopy(projectPath, dependencies) {
 	const directoriesToBeCopied = [];
 	const directoriesFound = [];
 
-	const pendingItems = _.keys(dependencies).map(name => ({ name }));
+	const pendingItems = dependencies.map(name => ({ name }));
 
 	while (pendingItems.length) {
 		const currentItem = pendingItems.shift();
 		console.debug(`searching for module: ${currentItem.name}`.blue);
 		const dependency = findDependency(currentItem);
-		if (dependency && ! _.includes(directoriesFound, dependency.directory)) {
+		if (dependency && !directoriesFound.includes(dependency.directory)) {
 			directoriesFound.push(dependency.directory);
-			if (dependency.isRoot && ! _.includes(directoriesToBeCopied, dependency.directory)) {
+			if (dependency.isRoot && !directoriesToBeCopied.includes(dependency.directory)) {
 				directoriesToBeCopied.push(dependency.directory);
 				console.debug(`    adding dependency: ${dependency.name}`);
 			}
-			_.forEach(dependency.dependencies, subDependency => {
+			dependency.dependencies.forEach(subDependency => {
 				pendingItems.push({
 					name:   subDependency,
 					parent: {
@@ -54,36 +77,40 @@ copier.execute = (projectPath, targetPath) => {
 			});
 		}
 	}
-	console.debug(`directoriesToBeCopied: ${JSON.stringify(directoriesToBeCopied, null, 2)}`.blue);
+	return directoriesToBeCopied;
 
-	_.forEach(directoriesToBeCopied, directory => {
-		const destPath = path.join(targetPath, directory.substring(projectPath.length));
-		console.debug(`copying to directory: ${destPath}`);
-		fs.copySync(directory, destPath, { overwrite: true });
-	});
-
+	/**
+	 * @param {object} metadata module metadata
+	 * @param {string} [name] module name
+	 * @returns {object} module metadata
+	 */
 	function findDependency(metadata, name) {
-		if (_.isNil(metadata)) {
+		if (metadata === null) {
 			return null;
 		}
 		name = name || metadata.name;
-		const parentDir = _.get(metadata, 'parent.directory', projectPath);
+		const parentDir = get(metadata, 'parent.directory', projectPath);
 		const directory = path.join(parentDir, NODE_MODULES, name);
 		console.debug(`    looking in dir: ${directory}`);
 		let dependencyPackageJson;
 		try {
-			dependencyPackageJson = require(path.join(directory, 'package.json'));
+			dependencyPackageJson = fs.readJsonSync(path.join(directory, 'package.json'));
 		} catch (err) {
 			console.debug(`     error: ${directory}`.red);
 			return findDependency(metadata.parent, name);
 		}
 		console.debug('    found module!'.green);
+		const dependencies = Object.keys(dependencyPackageJson.dependencies || {});
+		// include optional dependencies too
+		if (dependencyPackageJson.optionalDependencies) {
+			dependencies.push(...Object.keys(dependencyPackageJson.optionalDependencies));
+		}
 		return {
 			name,
 			directory,
-			dependencies: _.keys(dependencyPackageJson.dependencies),
-			isRoot:       projectPath === parentDir,
+			dependencies,
+			isRoot: projectPath === parentDir,
 		};
 	}
-};
+}
 
