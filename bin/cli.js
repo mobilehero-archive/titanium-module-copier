2b613dbc877356b34eed93504f314802653cb528
Chris Williams <chris.a.williams@gmail.com>
2019-05-20 10:52:17 -0400

    fix: fix usage from cli

--- bin/cli.js
+++ bin/cli.js
@@ -1,10 +1,14 @@
 #!/usr/bin/env node
 
 const args = process.argv.slice(2);
-if (args.length != 2) {
+if (args.length !== 2) {
 	console.error('copy-modules requires 2 parameters: projectPath and targetPath');
-	return 1;
-} else {
-	require('../index').execute(...args);
+	process.exit(1);
 }
 
+require('../index').execute(...args)
+	.then(() => process.exit(0))
+	.catch(e => {
+		console.error(e);
+		process.exit(1);
+	});
